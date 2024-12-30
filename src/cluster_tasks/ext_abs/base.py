2513b8ebdbbf508f749a3fa9973757bfadec7cd6
lexxai <lexxai@gmail.com>
2024-12-30 13:41:13 +0200

    core methods to abs

--- src/cluster_tasks/ext_abs/base.py
+++ src/cluster_tasks/ext_abs/base.py
@@ -23,16 +23,38 @@ class AbstractHandler(ABC):
     async def __aexit__(self, exc_type, exc_value, traceback):
         return True
 
-    def process(self, input_data: dict | None = None) -> dict:
-        """Process the input data."""
-        ...
-
-    async def aprocess(self, input_data: dict | None = None) -> dict:
-        """Process the input data."""
-        ...
+    @abstractmethod
+    def process(self, input_data: dict | None = None) -> dict: ...
 
     @abstractmethod
-    def get_version(self) -> dict: ...
+    async def aprocess(self, input_data: dict | None = None) -> dict: ...
 
     @abstractmethod
-    async def aget_version(self) -> dict: ...
+    def get_version_data(self) -> dict:
+        return {}
+
+    def get_version(self) -> dict:
+        return self.process(self.get_version_data())
+
+    async def aget_version(self) -> dict:
+        return await self.aprocess(self.get_version_data())
+
+    @abstractmethod
+    def get_ha_groups_data(self) -> dict:
+        return {}
+
+    def get_ha_groups(self):
+        return self.process(self.get_ha_groups_data())
+
+    async def aget_ha_groups(self):
+        return await self.aprocess(self.get_ha_groups_data())
+
+    @abstractmethod
+    def get_status_data(self, target_node: str) -> dict:
+        return {}
+
+    def get_status(self, target_node: str) -> dict:
+        return self.process(self.get_status_data(target_node))
+
+    async def aget_status(self, target_node: str) -> dict:
+        return await self.aprocess(self.get_status_data(target_node))
