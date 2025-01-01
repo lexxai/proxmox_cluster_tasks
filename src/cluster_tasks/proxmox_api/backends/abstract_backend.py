3ebe7596095012f0ed62ce466889102201e54bc4
lexxai <lexxai@gmail.com>
2025-01-01 13:31:28 +0200

    remove @abstractmethod

--- src/cluster_tasks/proxmox_api/backends/abstract_backend.py
+++ src/cluster_tasks/proxmox_api/backends/abstract_backend.py
@@ -2,12 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class ProxmoxBackend(ABC):
-    @abstractmethod
     def request(
         self, method: str, endpoint: str, params: dict = None, data: dict = None
     ) -> dict:
         """Perform a synchronous API request."""
-        ...
+        raise NotImplementedError("Sync request not implemented for this backend")
 
     async def async_request(
         self, method: str, endpoint: str, params: dict = None, data: dict = None
