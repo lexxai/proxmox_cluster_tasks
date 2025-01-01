2fc40931856adfd2998b7b787083acddda414b50
lexxai <lexxai@gmail.com>
2025-01-01 17:11:37 +0200

    register by names

--- src/ext_api/backends/registry.py
+++ src/ext_api/backends/registry.py
@@ -1,24 +1,69 @@
+import logging
+
 from ext_api.backends.backend_registry import (
     BackendRegistry,
     BackendType,
 )
-from ext_api.backends.backend_cli import ProxmoxCLIBackend, ProxmoxAsyncCLIBackend
-from ext_api.backends.backend_https import ProxmoxHTTPSBackend, ProxmoxAsyncHTTPSBackend
-from ext_api.backends.backend_ssh import (
-    ProxmoxSSHBackend,
-    ProxmoxAsyncSSHBackend,
-)
+
+logger = logging.getLogger(f"CT.{__name__}")
+
+BACKENDS_NAMES = ["https", "cli", "ssh"]
 
 
-def register_backends():
-    BackendRegistry.register_backend("https", BackendType.SYNC, ProxmoxHTTPSBackend)
-    BackendRegistry.register_backend(
-        "https", BackendType.ASYNC, ProxmoxAsyncHTTPSBackend
-    )
-    BackendRegistry.register_backend("cli", BackendType.SYNC, ProxmoxCLIBackend)
-    BackendRegistry.register_backend("cli", BackendType.ASYNC, ProxmoxAsyncCLIBackend)
-    BackendRegistry.register_backend("ssh", BackendType.SYNC, ProxmoxSSHBackend)
-    BackendRegistry.register_backend("ssh", BackendType.ASYNC, ProxmoxAsyncSSHBackend)
+def register_backends(names: list[str] | str = None):
+    if names is None:
+        names = BACKENDS_NAMES
+    if isinstance(names, str):
+        names = [names]
+    for name in names:
+        match name.strip().lower():
+            case "https":
+                try:
+                    from ext_api.backends.backend_https import (
+                        ProxmoxHTTPSBackend,
+                        ProxmoxAsyncHTTPSBackend,
+                    )
+
+                    BackendRegistry.register_backend(
+                        "https", BackendType.SYNC, ProxmoxHTTPSBackend
+                    )
+                    BackendRegistry.register_backend(
+                        "https", BackendType.ASYNC, ProxmoxAsyncHTTPSBackend
+                    )
+                except ImportError:
+                    logger.error("Failed to import HTTPS Backend")
+            case "cli":
+                try:
+                    from ext_api.backends.backend_cli import (
+                        ProxmoxCLIBackend,
+                        ProxmoxAsyncCLIBackend,
+                    )
+
+                    BackendRegistry.register_backend(
+                        "cli", BackendType.SYNC, ProxmoxCLIBackend
+                    )
+                    BackendRegistry.register_backend(
+                        "cli", BackendType.ASYNC, ProxmoxAsyncCLIBackend
+                    )
+                except ImportError:
+                    logger.error("Failed to import CLI Backend")
+            case "ssh":
+                try:
+                    from ext_api.backends.backend_ssh import (
+                        ProxmoxSSHBackend,
+                        ProxmoxAsyncSSHBackend,
+                    )
+
+                    BackendRegistry.register_backend(
+                        "ssh", BackendType.SYNC, ProxmoxSSHBackend
+                    )
+                    BackendRegistry.register_backend(
+                        "ssh", BackendType.ASYNC, ProxmoxAsyncSSHBackend
+                    )
+                except ImportError:
+                    logger.error("Failed to import SSH Backend")
+            case _:
+                logger.error(f"Unknown backend: {name}")
 
 
 def get_backends_names():
@@ -26,5 +71,5 @@ def get_backends_names():
 
 
 if __name__ == "__main__":
-    register_backends()
+    register_backends(["https", "cli"])
     print(get_backends_names())
