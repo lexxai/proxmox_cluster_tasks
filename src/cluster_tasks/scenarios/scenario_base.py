281a7b415c51d82344c7c1e5510c32a1ab638810
lexxai <lexxai@gmail.com>
2025-01-05 01:42:04 +0200

    ScenarioCloneTemplateVm cloned first VM

--- src/cluster_tasks/scenarios/scenario_base.py
+++ src/cluster_tasks/scenarios/scenario_base.py
@@ -1,12 +1,37 @@
+import asyncio
+import logging
+import time
 from abc import ABC, abstractmethod
 
 from ext_api.proxmox_api import ProxmoxAPI
+
+logger = logging.getLogger("CT.{__name__}")
 
 
 class ScenarioBase(ABC):
     def __init__(self, api: ProxmoxAPI):
         self.name = self.__class__.__name__
         self.api: ProxmoxAPI = api
+
+    @staticmethod
+    def get_status(api: ProxmoxAPI, node: str, upid: str):
+        result = api.nodes(node).tasks(upid).status.get(filter_keys="status")
+        return result
+
+    def wait_task_done(self, api: ProxmoxAPI, node: str, upid: str):
+        while self.get_status(api, node, upid) != "stopped":
+            logger.info("Waiting for task to finish...")
+            time.sleep(2)
+
+    @staticmethod
+    async def async_get_status(api: ProxmoxAPI, node: str, upid: str):
+        result = await api.nodes(node).tasks(upid).status.get(filter_keys="status")
+        return result
+
+    async def async_wait_task_done(self, api: ProxmoxAPI, node: str, upid: str):
+        while await self.async_get_status(api, node, upid) != "stopped":
+            logger.info("Waiting for task to finish...")
+            await asyncio.sleep(2)
 
     @abstractmethod
     def run(self):
--- src/cluster_tasks/scenarios/scenario_clone_template_vm.py
+++ src/cluster_tasks/scenarios/scenario_clone_template_vm.py
@@ -16,14 +16,29 @@ class ScenarioCloneTemplateVm(ScenarioBase):
     def configure(self, config):
         self.node = config.get("node")
         self.vmid = config.get("vmid")
-        self.newid = config.get("name")
-        self.full = config.get("full", True)
+        self.newid = config.get("newid")
+        self.name = config.get("name")
+        self.full = int(config.get("full", True))
 
     def run(self):
         print(f"Running Scenario Template: {self.template_name} at {self.source_node}")
         # Perform the specific API logic for this scenario
 
-        data = {"newid": self.newid, "name": self.name}
+        # data = {"newid": int(self.newid), "name": self.name, "full": self.full}
         with self.api as api:
-            result = api.nodes(self.node).qemu(self.vmid).clone.create(data=data)
-        logger.info(result)
+            present = (
+                api.nodes(self.node)
+                .qemu(self.newid)
+                .status.current.get(filter_keys="vmid")
+            )
+            logger.info(f"result: {present} {self.newid} {self.newid==present}")
+            if present and int(present) == int(self.newid):
+                logger.info(f"VM {self.newid} already exists - Deleteting...")
+                upid = api.nodes(self.node).qemu(self.newid).delete()
+                logger.info(f"result: {upid}")
+                self.wait_task_done(api, self.node, upid)
+            data = {"newid": int(self.newid), "name": self.name, "full": self.full}
+            upid = api.nodes(self.node).qemu(self.vmid).clone.create(data=data)
+            logger.info(f"result: {upid}, data: {data}")
+            self.wait_task_done(api, self.node, upid)
+            logger.info(f"VM {self.newid} created")
