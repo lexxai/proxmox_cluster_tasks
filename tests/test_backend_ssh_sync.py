7294100c68edd61624c5d981b5ea6f7297f86864
lexxai <lexxai@gmail.com>
2025-01-09 16:55:16 +0200

    0.6.2 BackendRequestSSHTest init

--- tests/test_backend_ssh_sync.py
+++ tests/test_backend_ssh_sync.py
@@ -1,5 +1,4 @@
 import unittest
-from importlib.metadata import entry_points
 from unittest.mock import patch, MagicMock
 import subprocess
 
@@ -7,38 +6,17 @@ from ext_api.backends.backend_registry import BackendRegistry
 from ext_api.backends.registry import register_backends
 
 
-class BackendRequestTest(unittest.TestCase):
+class BackendRequestSSHTest(unittest.TestCase):
     def setUp(self):
-        # class MockBackend:
-        # def format_command(self, endpoint, params, method, data, endpoint_params):
-        #     # Simulate different commands based on the backend logic
-        #     match endpoint:
-        #         case "/version":
-        #             return 'echo {"release":"8.3","repoid":"3e76eec21c4a14a7","version":"8.3.2"}'
-        #         case _:
-        #             return "mock_command"
-
-        # def result_analyze(self, output, error, exit_status):
-        #     # Simulate analyzing the result and returning a response
-        #     return {
-        #         "response": output,
-        #         "status_code": exit_status,
-        #         "success": exit_status == 0,
-        #     }
-
-        # self.backend = MockBackend()
-        register_backends()
-        backend_name = "cli"
-        backend_cls = BackendRegistry.get_backend(backend_name)
-        self.backend_cli = backend_cls(entry_point="echo")
         backend_name = "ssh"
+        register_backends(backend_name)
         backend_cls = BackendRegistry.get_backend(backend_name)
-        self.backend_ssh = backend_cls(
+        self.backend = backend_cls(
             entry_point="echo", hostname="localhost", username="fake_user"
         )
 
     @patch("subprocess.run")
-    def test_request_success_cli_backend_sync(self, mock_subprocess_run):
+    def test_request_success_ssh_backend_sync(self, mock_subprocess_run):
         # Mock a successful subprocess run
         mock_process = MagicMock()
         mock_process.stdout = (
@@ -52,7 +30,7 @@ class BackendRequestTest(unittest.TestCase):
             "method": "get",
             "endpoint": "version",
         }
-        result = self.backend_cli.request(**request_params)
+        result = self.backend.request(**request_params)
         self.assertIsNotNone(result)
         response = result.get("response")
         self.assertIsNotNone(response)
@@ -63,7 +41,7 @@ class BackendRequestTest(unittest.TestCase):
         self.assertTrue(result["success"])
 
     @patch("subprocess.run")
-    def test_request_failure_cli_backend_sync(self, mock_subprocess_run):
+    def test_request_failure_ssh_backend_sync(self, mock_subprocess_run):
         # Mock a subprocess that raises CalledProcessError
         mock_subprocess_run.side_effect = subprocess.CalledProcessError(
             returncode=1, cmd="echo", output="error output", stderr="error"
@@ -72,18 +50,18 @@ class BackendRequestTest(unittest.TestCase):
             "method": "get",
             "endpoint": "version",
         }
-        result = self.backend_cli.request(**request_params)
+        result = self.backend.request(**request_params)
         self.assertIsNone(result["response"])
         self.assertEqual(result["status_code"], 1)
         self.assertFalse(result["success"])
 
-    def test_request_no_command_cli_backend_sync(self):
+    def test_request_no_command_ssh_backend_sync(self):
         # Test behavior when format_command returns None
         request_params = {
             "method": "get",
             "endpoint": "",
         }
-        result = self.backend_cli.request(**request_params)
+        result = self.backend.request(**request_params)
         self.assertIsNone(result["response"])
         self.assertEqual(result["status_code"], -1)
         self.assertFalse(result["success"])
