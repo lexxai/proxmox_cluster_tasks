ad2ae375e9bff48c0f8da36d260862d6192eca2d
lexxai <lexxai@gmail.com>
2025-01-10 16:06:45 +0200

    0.6.4    connect      raise NotImplementedError("Connect not implemented for this backend")

--- src/ext_api/backends/backend_abstract.py
+++ src/ext_api/backends/backend_abstract.py
@@ -4,6 +4,9 @@ from abc import ABC, abstractmethod
 class ProxmoxBackend(ABC):
     def __init__(self, *args, **kwargs): ...
 
+    def connect(self, *args, **kwargs):
+        raise NotImplementedError("Connect not implemented for this backend")
+
     def request(self, *args, **kwargs) -> dict:
         """Perform a synchronous API request."""
         raise NotImplementedError("Sync request not implemented for this backend")
